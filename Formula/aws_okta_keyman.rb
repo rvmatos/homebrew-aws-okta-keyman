9a6555a8db9c9d91f78cac94e3a41c5e47409063
Nathan V <nathan.v@gmail.com>
2019-12-09 15:22:56 -0800

    v0.7.4

--- Formula/aws_okta_keyman.rb
+++ Formula/aws_okta_keyman.rb
@@ -9,8 +9,8 @@ class AwsOktaKeyman < Formula
 
   desc "AWS Okta Keyman: An AWS/Okta connector that pulls AWS API keys using an Okta SAML assertion."
   homepage "https://github.com/nathan-v/aws_okta_keyman"
-  url "https://files.pythonhosted.org/packages/57/50/d8a520c68b9218febf1ea33a806006e638b7ab7e8978692f871300c44b9c/aws_okta_keyman-0.7.0.tar.gz"
-  sha256 "65642b2040afd10ca04c08e949e3a31dc1dd669030a803890efcdda74a0e5962"
+  url "https://files.pythonhosted.org/packages/75/0e/2534605569123a948e99ece991c82a4f2d0a1f0c19e27a1bca45fc8842b6/aws_okta_keyman-0.7.4.tar.gz"
+  sha256 "62d537f7cff61c3cae940f2af06b0583c127b0b0d2fbc34e6cae09f5069da5c0"
 
   resource "beautifulsoup4" do
     url "https://files.pythonhosted.org/packages/86/cd/495c68f0536dcd25f016e006731ba7be72e072280305ec52590012c1e6f2/beautifulsoup4-4.8.1.tar.gz"
@@ -18,13 +18,13 @@ class AwsOktaKeyman < Formula
   end
 
   resource "boto3" do
-    url "https://files.pythonhosted.org/packages/b3/58/cf81bb738c15cff4314951282c71caac61ef6ff17b1bc8793717c8eb2b22/boto3-1.9.252.tar.gz"
-    sha256 "b169b634089d3a73d65f4c4573989d76e89cb10950669883eb82519fbadc8514"
+    url "https://files.pythonhosted.org/packages/70/cf/f42c9a8d2758463bd25cf89bf39888a4065533d62e248ea43310fad32232/boto3-1.10.35.tar.gz"
+    sha256 "5cad688930e22c7edf434c34747cac058ae0de394999155e07ff848feb90f53d"
   end
 
   resource "botocore" do
-    url "https://files.pythonhosted.org/packages/33/68/095924645b88d76ec6d2f928316f2a82e3108315ba78e009c46090c3bc92/botocore-1.12.252.tar.gz"
-    sha256 "30c43c2f88be51a2bc9ed3b8c0d3093d3865698576a6dda557cd04734dc73eff"
+    url "https://files.pythonhosted.org/packages/82/0c/8c5189e381ed63c6cbe449385a50100a91e4f883be2b2fd408e155ca7e2f/botocore-1.13.35.tar.gz"
+    sha256 "64db64cbb461a5970dcfed63a6d82729fce7850739f2322ff0fccf88e9259ecf"
   end
 
   resource "bs4" do
@@ -33,8 +33,8 @@ class AwsOktaKeyman < Formula
   end
 
   resource "certifi" do
-    url "https://files.pythonhosted.org/packages/62/85/7585750fd65599e88df0fed59c74f5075d4ea2fe611deceb95dd1c2fb25b/certifi-2019.9.11.tar.gz"
-    sha256 "e4f3620cfea4f83eedc95b24abd9cd56f3c4b146dd0177e83a21b4eb49e21e50"
+    url "https://files.pythonhosted.org/packages/41/bf/9d214a5af07debc6acf7f3f257265618f1db242a3f8e49a9b516f24523a6/certifi-2019.11.28.tar.gz"
+    sha256 "25b64c7da4cd7479594d035c08c2d809eb4aab3a26e5a990ea98cc450c320f1f"
   end
 
   resource "chardet" do
@@ -42,9 +42,9 @@ class AwsOktaKeyman < Formula
     sha256 "84ab92ed1c4d4f16916e05906b6b75a6c0fb5db821cc65e70cbd64a3e2a5eaae"
   end
 
-  resource "colorama" do
-    url "https://files.pythonhosted.org/packages/76/53/e785891dce0e2f2b9f4b4ff5bc6062a53332ed28833c7afede841f46a5db/colorama-0.4.1.tar.gz"
-    sha256 "05eed71e2e327246ad6b38c540c4a3117230b19679b875190486ddd2d721422d"
+  resource "colorlog" do
+    url "https://files.pythonhosted.org/packages/fc/30/6ba1282b773e9f44d9cfaafa38b6cc180441307c5fe0edd8db13a8903e3f/colorlog-4.0.2.tar.gz"
+    sha256 "3cf31b25cbc8f86ec01fef582ef3b840950dea414084ed19ab922c8b493f9b42"
   end
 
   resource "configparser" do
@@ -58,8 +58,8 @@ class AwsOktaKeyman < Formula
   end
 
   resource "future" do
-    url "https://files.pythonhosted.org/packages/3f/bf/57733d44afd0cf67580658507bd11d3ec629612d5e0e432beb4b8f6fbb04/future-0.18.1.tar.gz"
-    sha256 "858e38522e8fd0d3ce8f0c1feaf0603358e366d5403209674c7b617fa0c24093"
+    url "https://files.pythonhosted.org/packages/45/0b/38b06fd9b92dc2b68d58b75f900e97884c45bedd2ff83203d933cf5851c9/future-0.18.2.tar.gz"
+    sha256 "b1bead90b70cf6ec3f0710ae53a525360fa360d306a86583adc6bf83a4db537d"
   end
 
   resource "idna" do
@@ -67,14 +67,24 @@ class AwsOktaKeyman < Formula
     sha256 "c357b3f628cf53ae2c4c05627ecc484553142ca23264e593d327bcde5e9c3407"
   end
 
+  resource "importlib-metadata" do
+    url "https://files.pythonhosted.org/packages/9b/42/86a31ce5fd7e3c5fc9071cec95d0aab11deb2fd63eed27315f520d120bfd/importlib_metadata-1.2.0.tar.gz"
+    sha256 "41e688146d000891f32b1669e8573c57e39e5060e7f5f647aa617cd9a9568278"
+  end
+
   resource "jmespath" do
     url "https://files.pythonhosted.org/packages/2c/30/f0162d3d83e398c7a3b70c91eef61d409dea205fb4dc2b47d335f429de32/jmespath-0.9.4.tar.gz"
     sha256 "bde2aef6f44302dfb30320115b17d030798de8c4110e28d5cf6cf91a7a31074c"
   end
 
-  resource "logutils" do
-    url "https://files.pythonhosted.org/packages/49/b2/b57450889bf73da26027f8b995fd5fbfab258ec24ef967e4c1892f7cb121/logutils-0.3.5.tar.gz"
-    sha256 "bc058a25d5c209461f134e1f03cab637d66a7a5ccc12e593db56fbb279899a82"
+  resource "keyring" do
+    url "https://files.pythonhosted.org/packages/e2/ca/f7caecaac22e06cf4de0e5144a4f2a47251f8fee598f84d5f3fa1d8709a7/keyring-20.0.0.tar.gz"
+    sha256 "fc9cadedae35b77141f670f84c10a657147d2e526348698c93dd77f039979729"
+  end
+
+  resource "more-itertools" do
+    url "https://files.pythonhosted.org/packages/4e/b2/e9e512cccde6c54bf66a8e5820a2af779eb8235028627002ca90d4f75bea/more-itertools-8.0.2.tar.gz"
+    sha256 "b84b238cce0d9adad5ed87e745778d20a3f8487d0f0cb8b8a586816c7496458d"
   end
 
   resource "python-dateutil" do
@@ -83,13 +93,8 @@ class AwsOktaKeyman < Formula
   end
 
   resource "PyYAML" do
-    url "https://files.pythonhosted.org/packages/e3/e8/b3212641ee2718d556df0f23f78de8303f068fe29cdaa7a91018849582fe/PyYAML-5.1.2.tar.gz"
-    sha256 "01adf0b6c6f61bd11af6e10ca52b7d4057dd0be0343eb9283c878cf3af56aee4"
-  end
-
-  resource "rainbow_logging_handler" do
-    url "https://files.pythonhosted.org/packages/3d/15/aa3cd5c46c709b2bc45e3f8612b6484bbc869d43f92dfc4a86b65e6a240a/rainbow_logging_handler-2.2.2.zip"
-    sha256 "a504c20f1e074cc7f5a5b986c08eb6fb495c9153cffb36b5fa0a587de3a8db63"
+    url "https://files.pythonhosted.org/packages/8d/c9/e5be955a117a1ac548cdd31e37e8fd7b02ce987f9655f5c7563c656d5dcb/PyYAML-5.2.tar.gz"
+    sha256 "c0ee8eca2c582d29c3c2ec6e2c4f703d1b7f1fb10bc72317355a746057e7346c"
   end
 
   resource "requests" do
@@ -103,18 +108,23 @@ class AwsOktaKeyman < Formula
   end
 
   resource "six" do
-    url "https://files.pythonhosted.org/packages/dd/bf/4138e7bfb757de47d1f4b6994648ec67a51efe58fa907c1e11e350cddfca/six-1.12.0.tar.gz"
-    sha256 "d16a0141ec1a18405cd4ce8b4613101da75da0e9a7aec5bdd4fa804d0e0eba73"
+    url "https://files.pythonhosted.org/packages/94/3e/edcf6fef41d89187df7e38e868b2dd2182677922b600e880baad7749c865/six-1.13.0.tar.gz"
+    sha256 "30f610279e8b2578cab6db20741130331735c781b56053c59c4076da27f06b66"
   end
 
   resource "soupsieve" do
-    url "https://files.pythonhosted.org/packages/7f/4e/95a13527e18b6f1a15c93f1c634b86d5fa634c5619dce695f4e0cd68182f/soupsieve-1.9.4.tar.gz"
-    sha256 "605f89ad5fdbfefe30cdc293303665eff2d188865d4dbe4eb510bba1edfbfce3"
+    url "https://files.pythonhosted.org/packages/92/cf/57dfed8a00f4ba33af3a6615d693bb65a19a11e26ab13293f62359216417/soupsieve-1.9.5.tar.gz"
+    sha256 "e2c1c5dee4a1c36bcb790e0fabd5492d874b8ebd4617622c4f6a731701060dda"
   end
 
   resource "urllib3" do
-    url "https://files.pythonhosted.org/packages/ff/44/29655168da441dff66de03952880c6e2d17b252836ff1aa4421fba556424/urllib3-1.25.6.tar.gz"
-    sha256 "9a107b99a5393caf59c7aa3c1249c16e6879447533d0887f4336dde834c7be86"
+    url "https://files.pythonhosted.org/packages/ad/fc/54d62fa4fc6e675678f9519e677dfc29b8964278d75333cf142892caf015/urllib3-1.25.7.tar.gz"
+    sha256 "f3c5fd51747d450d4dcf6f923c81f78f811aab8205fda64b0aba34a4e48b0745"
+  end
+
+  resource "zipp" do
+    url "https://files.pythonhosted.org/packages/57/dd/585d728479d97d25aeeb9aa470d36a4ad8d0ba5610f84e14770128ce6ff7/zipp-0.6.0.tar.gz"
+    sha256 "3718b1cbcd963c7d4c5511a8240812904164b7f381b647143a89d3b98f9bcd8e"
   end
 
 end
